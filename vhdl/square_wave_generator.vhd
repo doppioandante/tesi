7ad726d17661f30bb15e97eebc79f6bcb4fb24f1
Enrico Lumetti <doppioandante@gmail.com>
2018-10-29 13:19:40 +0100

    Add NCO-based square wave generator

---
+++ vhdl/square_wave_generator.vhd
@@ -0,0 +1,83 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std_unsigned.all;
+use ieee.math_real.log2;
+use ieee.math_real.ceil;
+
+entity square_wave_generator is
+    generic(
+        -- how often the phase is updated
+        -- TODO: generalize the architecture code
+        -- another counter_limit must be used in case of
+        -- update_frequency different from clock frequency
+        -- and add clock_frequency to the generic parameters
+        update_frequency: positive := 100_000_000;
+        -- how often the output sample is produced
+        output_frequency: positive;
+        -- bits of the output sample
+        sample_bits: positive;
+        -- bits used for the fixed point phase representation
+        phase_bits: positive
+    );
+    port(
+        clock: in std_logic;
+
+        phase_input_enable: in std_logic;
+        phase_step: in std_logic_vector(phase_bits-1 downto 0);
+
+        output_enable: out std_logic := '0';
+        output_sample: out std_logic_vector(sample_bits-1 downto 0)
+    );
+end square_wave_generator;
+
+architecture behavioural of square_wave_generator is
+    constant counter_limit: positive := update_frequency/output_frequency;
+    constant counter_bits: positive := positive(ceil(log2(real(counter_limit))));
+
+    signal counter: std_logic_vector(counter_bits-1 downto 0) := (others => '0');
+
+    signal phase: std_logic_vector(phase_bits-1 downto 0) := (others => '0');
+    signal m_phase_step: std_logic_vector(phase_bits-1 downto 0) := (others => '0');
+begin
+    assert 2**sample_bits <= counter_limit
+        report "Output frequency is not high enough to represent the input"
+        severity error;
+
+    process (counter, phase)
+    begin
+        if counter = 0 then
+            output_enable <= '1';
+            -- use first bit of the phase to determine the square wave
+            if phase(phase'left) = '1' then
+                output_sample <= (others => '1');
+            else
+                output_sample <= (others => '0');
+            end if;
+        else
+            output_enable <= '0';
+        end if;
+    end process;
+
+    process (clock, phase_input_enable, phase_step)
+    begin
+        if rising_edge(clock) then
+            if phase_input_enable then
+                m_phase_step <= phase_step;
+            end if;
+        end if;
+    end process;
+
+    process (clock, counter, phase, phase_step)
+    begin
+        if rising_edge(clock) then
+            -- TODO...
+            phase <= phase + phase_step;
+            if counter = counter_limit - 1 then
+                counter <= (others => '0');
+            else
+                counter <= counter + 1;
+            end if;
+        end if;
+    end process;
+
+end behavioural;
---
+++ vhdl/square_wave_generator_tb.vhd
@@ -0,0 +1,57 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.numeric_std_unsigned.all;
+use ieee.math_real.log2;
+use ieee.math_real.ceil;
+
+entity square_wave_generator_tb is
+end square_wave_generator_tb;
+
+architecture testbench of square_wave_generator_tb is
+    constant clock_period: time := 10 ns;
+    -- 10 MHz sampling frequency (100ns sampling period)
+    -- such an high sampling rate will allow only 10 datapoints
+    constant sampling_frequency: positive := 10_000_000;
+    constant sample_bits: positive := 1; -- only one needed for a square wave
+
+    constant wave_frequency: positive := 440;
+    constant phase_bits: positive := 32;
+    constant step_phase: positive := positive(ceil(real(2**phase_bits) * real(wave_frequency)/real(100_000_000)));
+
+    signal clock: std_logic;
+
+    signal input_enable: std_logic;
+    signal sample: std_logic_vector(sample_bits-1 downto 0);
+begin
+    clock_proc: entity work.tb_clock_process generic map (clock_period)
+    port map(
+        clock => clock
+    );
+
+    uut: entity work.square_wave_generator
+    generic map (
+        update_frequency => 100_000_000,
+        output_frequency => sampling_frequency,
+        sample_bits => sample_bits,
+        phase_bits => phase_bits
+    )
+    port map (
+        clock => clock,
+        phase_input_enable => input_enable,
+        phase_step => to_std_logic_vector(step_phase, phase_bits),
+        output_enable => open,
+        output_sample => sample
+    );
+
+    test_process: process
+    begin
+        wait for clock_period;
+        input_enable <= '1';
+        wait for clock_period;
+        input_enable <= '0';
+        wait for 4 ms;
+
+        std.env.stop;
+    end process test_process;
+end testbench;
