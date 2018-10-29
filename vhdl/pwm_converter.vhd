8047c7f95b7acae9821472ea92a01afb69036d4a
Enrico Lumetti <doppioandante@gmail.com>
2018-10-29 13:16:05 +0100

    Add pwm output generator

---
+++ vhdl/pwm_converter.vhd
@@ -0,0 +1,69 @@
+-- pwm converter
+-- converts the input sample to PWM
+-- where logic '1' is attained by driving the output at high impedance
+-- when no output is available, outputs a square wave (no sound)
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.numeric_std_unsigned.all;
+use ieee.math_real.log2;
+use ieee.math_real.ceil;
+
+entity pwm_converter is
+    generic (
+        clock_frequency: positive := 100_000_000; -- in Hz
+        --output_pulse_frequency: positive := clock_frequency; -- in Hz
+        input_sampling_frequency: positive;
+        input_sample_bits: positive
+    );
+    port (
+        clock: in std_logic;
+
+        -- a sample is available
+        input_enable: in std_logic;
+        -- input sample to be converted to PWM
+        sample: in std_logic_vector(input_sample_bits-1 downto 0);
+
+        -- pwm output
+        pwm_out: out std_logic := 'Z'
+    );
+end pwm_converter;
+
+architecture behavioural of pwm_converter is
+    -- TODO: account for output_pulse_frequency
+    constant counter_limit: positive := clock_frequency/input_sampling_frequency;
+    constant counter_bits: positive := positive(ceil(log2(real(counter_limit))));
+
+    signal counter: std_logic_vector(counter_bits-1 downto 0) := (others => '0');
+begin
+    -- assert output_pulse_frequency < input_sampling_frequency
+
+    -- this assert may be to restrictive
+    -- e.g. 10 possible sample values can be represented with 4 bits
+    -- but 2**4 >= 10
+    assert 2**input_sample_bits <= counter_limit
+        report "Output pwm frequency is not high enough to represent the input"
+        severity error;
+
+    process (counter, sample, input_enable)
+    begin
+        if (input_enable = '1' and counter <= sample)
+            or (input_enable /= '1' and counter <= counter_limit/2) then
+            pwm_out <= 'Z';
+        else
+            pwm_out <= '0';
+        end if;
+    end process;
+
+    process (clock, counter)
+    begin
+        if rising_edge(clock) then
+            if counter = counter_limit - 1 then
+                counter <= (others => '0');
+            else
+                counter <= counter + 1;
+            end if;
+        end if;
+    end process;
+end behavioural;
---
+++ vhdl/pwm_converter_tb.vhd
@@ -0,0 +1,70 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std_unsigned.all;
+use std.textio.all;
+
+entity pwm_converter_tb is
+end pwm_converter_tb;
+
+architecture testbench of pwm_converter_tb is
+    constant clock_period: time := 10 ns;
+    -- 10 MHz sampling frequency (100ns sampling period)
+    -- such an high sampling rate will allow only 10 datapoints
+    -- counter_bits will be 3
+    constant sampling_frequency: positive := 10_000_000;
+    constant sample_bits: positive := 3;
+    signal clock: std_logic;
+
+    signal input_enable: std_logic;
+    signal sample: std_logic_vector(sample_bits-1 downto 0);
+
+    signal pwm_out: std_logic;
+
+    signal stop_write: boolean := false;
+begin
+    clock_proc: entity work.tb_clock_process generic map (clock_period)
+    port map(
+        clock => clock
+    );
+
+    uut: entity work.pwm_converter
+    generic map (
+        input_sampling_frequency => sampling_frequency,
+        input_sample_bits => sample_bits
+    )
+    port map (
+        clock => clock,
+        input_enable => input_enable,
+        sample => sample,
+        pwm_out => pwm_out
+    );
+
+    test_process: process
+    begin
+        input_enable <= '1';
+        sample <= 3b"111"; -- (others => '0');
+        wait for clock_period * 50;
+        input_enable <= '0';
+        wait for clock_period * 50;
+
+        stop_write <= true;
+    end process test_process;
+
+    write_process: process
+        file output_file: text;
+        variable pwm_line: line;
+    begin
+        file_open(output_file, "pwm_out.txt", write_mode);
+        -- write out pwm sampling frequency
+        write(pwm_line, positive'image(100_000_000), left, 32);
+        writeline(output_file, pwm_line);
+        while not stop_write loop 
+            write(pwm_line, pwm_out, right, 1);
+            writeline(output_file, pwm_line);
+            wait for clock_period;
+        end loop;
+
+        file_close(output_file);
+        std.env.stop;
+    end process write_process;
+end testbench;
