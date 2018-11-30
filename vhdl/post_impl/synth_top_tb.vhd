b10e03e4a2ba59033a4cfb5d17bf6d1e10f41309
Enrico Lumetti <doppioandante@gmail.com>
2018-11-30 23:43:44 +0100

    Restore vhdl2008 testbench

--- vhdl/post_impl/synth_top_tb.vhd
+++ vhdl/post_impl/synth_top_tb.vhd
@@ -1,6 +1,5 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
 use std.textio.all;
 
 entity synth_top_tb is
@@ -15,14 +14,10 @@ architecture testbench of synth_top_tb is
     signal pwm_out: std_logic;
     signal stop_write: boolean := false;
 begin
-    clock_process: process
-    begin
-        clock <= '1';
-        while not stop_write loop
-            wait for clock_period/2;
-            clock <= not clock;
-        end loop;
-    end process clock_process;
+    clock_proc: entity work.tb_clock_process generic map (clock_period)
+    port map(
+        clock => clock
+    );
 
     uut: entity work.synth_top
     port map(
@@ -34,14 +29,12 @@ begin
 
     test_process: process
         -- TODO: factor out from uart_midi_link_tb
-        procedure send_byte(byte: in integer range 0 to 127) is
-            variable byte_vec: std_logic_vector(7 downto 0);
+        procedure send_byte(byte: in std_logic_vector(7 downto 0)) is
         begin
-            byte_vec := std_logic_vector(to_unsigned(byte, 8));
             uart_in <= '0';
             wait for bit_period;
-            for i in byte_vec'reverse_range loop
-                uart_in <= byte_vec(i);
+            for i in byte'reverse_range loop
+                uart_in <= byte(i);
                 wait for bit_period;
             end loop;
             uart_in <= '1';
@@ -50,19 +43,19 @@ begin
     begin
         uart_in <= '1';
 
-        send_byte(90); -- note on
-        send_byte(69); -- A 440Hz
-        send_byte(50);
+        send_byte(8x"90"); -- note on
+        send_byte(8d"69"); -- A 440Hz
+        send_byte(8x"50");
 
-        send_byte(90); -- note on
-        send_byte(73);
-        send_byte(50);
+        send_byte(8x"90"); -- note on
+        send_byte(8d"73");
+        send_byte(8x"50");
 
         wait for 3 ms;
  
-        send_byte(80); -- note off
-        send_byte(69); -- A 440Hz
-        send_byte(50);
+        send_byte(8x"80"); -- note off
+        send_byte(8d"69"); -- A 440Hz
+        send_byte(8x"50");
  
         wait for 100 us;
 
@@ -79,11 +72,12 @@ begin
         writeline(output_file, pwm_line);
         wait until rising_edge(clock);
         while not stop_write loop
-            write(pwm_line, std_logic'image(pwm_out), right, 1);
+            write(pwm_line, pwm_out, right, 1);
             writeline(output_file, pwm_line);
             wait for clock_period;
         end loop;
 
         file_close(output_file);
+        std.env.stop;
     end process write_process;
 end testbench;
