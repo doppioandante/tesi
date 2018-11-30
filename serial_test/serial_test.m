cfb9ba730b9bc177314bd1a1507849cd65241cce
Enrico Lumetti <doppioandante@gmail.com>
2018-11-30 18:17:49 +0100

    Polyphonic erial test

--- serial_test/serial_test.m
+++ serial_test/serial_test.m
@@ -10,8 +10,13 @@ s_out = serial(serial_port, 'BaudRate', 31250);
 fopen(s_out);
 % 90: noteon, 45 is A @ 440Hz, 50: velocity
 note_on = hex2dec(['90';'45'; '50']);
+note_on_2 = hex2dec(['90';'49'; '50']);
 note_off = hex2dec(['89'; '45'; '50']);
+note_off_2 = hex2dec(['89'; '49'; '50']);
 fwrite(s_out, note_on);
+fwrite(s_out, note_on_2);
 pause(2);
 fwrite(s_out, note_off);
+pause(1);
+fwrite(s_out, note_off_2);
 fclose(s_out);
