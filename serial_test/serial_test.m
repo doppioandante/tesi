78049f4032e121bdaa8a3b6e3ec402d778605956
Enrico Lumetti <doppioandante@gmail.com>
2018-12-01 15:00:03 +0100

    Workaround for serial testing with MATLAB on linux

--- serial_test/serial_test.m
+++ serial_test/serial_test.m
@@ -3,7 +3,12 @@ serial_port = '/dev/ttyUSB1';
 if ispc
     system(['win_set_baud.exe ' serial_port]);
 else
-    system(['./set_baud ' serial_port]);
+    % FIXME: set_baud doesn't work under matlab (ioctl doesn't set the
+    % precise baud rate)
+    % The windows program under wine works though...
+    system(['wine win_set_baud.exe ' serial_port]);
+    %system('gcc set_baud.c -o set_baud');
+    %system(['./set_baud ' serial_port]);
 end
 
 s_out = serial(serial_port, 'BaudRate', 31250);
@@ -14,6 +19,7 @@ note_on_2 = hex2dec(['90';'49'; '50']);
 note_off = hex2dec(['89'; '45'; '50']);
 note_off_2 = hex2dec(['89'; '49'; '50']);
 fwrite(s_out, note_on);
+pause(1);
 fwrite(s_out, note_on_2);
 pause(2);
 fwrite(s_out, note_off);
