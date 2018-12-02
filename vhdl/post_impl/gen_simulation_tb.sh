d62fd1c50bb4c8bd5a566836f5c122a43523ed6c
Enrico Lumetti <doppioandante@gmail.com>
2018-12-02 18:49:17 +0100

    Update post-impl simulation generation

--- vhdl/post_impl/gen_simulation_tb.sh
+++ vhdl/post_impl/gen_simulation_tb.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-cp ../synth_top_tb.vhd .
-cp ../tb_clock_process.vhd .
-ghdl -i --std=08 --no-vital-checks -Pxilinx-vivado/unisim/v08 post_route.vhd tb_clock_process.vhd synth_top_tb.vhd
+cp ../synth_top_tb.vhd  ../tb_clock_process.vhd ../tb_uart_pkg.vhd ../tb_logger.vhd .
+ghdl -i --std=08 --no-vital-checks -Pxilinx-vivado/unisim/v08 post_route.vhd tb_clock_process.vhd tb_uart_pkg.vhd tb_logger.vhd synth_top_tb.vhd
 ghdl -m --std=08 --no-vital-checks -Pxilinx-vivado/unisim/v08 -frelaxed-rules synth_top_tb
